e1b35f06679c3440d01ebb26a22375b358fe135f
Kofi-Astro <opokukelvin29@gmail.com>
2020-09-05 00:03:39 +0000

    Added Pictures

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,53 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           primarySwatch: Colors.purple, accentColor: Colors.deepOrange),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('List App'),
-        ),
-        body: Text('This is me and I am back'),
-      ),
+          backgroundColor: Colors.grey[200],
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.all(0),
+              children: [
+                UserAccountsDrawerHeader(
+                  accountName: Text('Kelvin Agyemang'),
+                  accountEmail: Text("opokukelvin29@gmail.com"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: AssetImage('assets/kay.jpg'),
+                  ),
+                  decoration: BoxDecoration(color: Colors.purple),
+                ),
+                ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text('Kelvin Agyemang'),
+                  subtitle: Text('Developer'),
+                  trailing: Icon(Icons.edit),
+                ),
+                ListTile(
+                    leading: Icon(Icons.email),
+                    title: Text("Email"),
+                    subtitle: Text("opokukelvin29@gmail.com"),
+                    trailing: Icon(Icons.edit))
+              ],
+            ),
+          ),
+          appBar: AppBar(
+            title: Text('List App'),
+          ),
+          body: Container(
+            child: ListView(
+              children: [
+                Card(
+                  child: Column(
+                    children: [Image.asset('assets/Grace.jpg')],
+                  ),
+                ),
+                Image.asset('assets/love.jpg'),
+                Text('This is me and I am back'),
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.near_me),
+            onPressed: () {},
+          )),
     );
   }
 }
